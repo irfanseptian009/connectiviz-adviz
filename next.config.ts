89987bc9ecc5b26cf6c6a28dc32eadca3459bfe6
irfan septian <ivanseftian79@gmai.com>
2025-03-21 14:22:01 +0700

    fix: deleted turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
-module.exports = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
-}
+};
+
+export default nextConfig;
