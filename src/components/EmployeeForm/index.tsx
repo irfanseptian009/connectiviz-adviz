394253b0c7960ded1d33e39cb7a8a201017a58ff
irfan septian <ivanseftian79@gmai.com>
2025-06-18 12:48:04 +0700

    fix employeeCreatModdal

---
+++ src/components/EmployeeForm/index.tsx
@@ -0,0 +1,8 @@
+import BusinessUnitModal from "./BusinessUnitModal";
+import DivisionModal from "./DivisionModal";
+import DivisionTree from "./DivisionTree";
+import NonFormalEducationArray from "./NonFormalEducationArray";
+import UserSelectModal from "./UserSelectModal";
+
+
+export { BusinessUnitModal, DivisionModal, DivisionTree, NonFormalEducationArray, UserSelectModal };
