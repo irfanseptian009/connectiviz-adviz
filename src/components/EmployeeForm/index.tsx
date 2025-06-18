3bb80da40f86b37fb6969927db52c21efba9fcda
irfan septian <ivanseftian79@gmai.com>
2025-06-18 13:00:32 +0700

    fix components createemplyee

--- src/components/EmployeeForm/index.tsx
+++
@@ -1,8 +0,0 @@
-import BusinessUnitModal from "./BusinessUnitModal";
-import DivisionModal from "./DivisionModal";
-import DivisionTree from "./DivisionTree";
-import NonFormalEducationArray from "./NonFormalEducationArray";
-import UserSelectModal from "./UserSelectModal";
-
-
-export { BusinessUnitModal, DivisionModal, DivisionTree, NonFormalEducationArray, UserSelectModal };
