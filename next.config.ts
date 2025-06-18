515723586a6fdc4c823106da029326217af32f22
irfan septian <ivanseftian79@gmai.com>
2025-06-18 11:40:50 +0700

    Fix import path using relative path and fix next.config.ts formatting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */  env: {
+  /* config options here */
+  env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://connectiviz-be.vercel.app/api",
   },
   images: {
